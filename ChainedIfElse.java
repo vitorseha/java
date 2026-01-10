0eeed39fa61a0f06bf486b3cb4f6449051acf54e
Vitor Serra <vserra.contact@gmail.com>
2026-01-10 13:43:03 -0300

    exercises(java): chained if and else.

---
+++ ChainedIfElse.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+public class ChainedIfElse {
+    public static void main(String[] args) {
+        double grade;
+        grade = Double.parseDouble(
+            JOptionPane.showInputDialog("Enter the grade")
+        );
+
+        if (grade >= 90) {
+            JOptionPane.showMessageDialog(null, "Congratulations");
+            JOptionPane.showMessageDialog(null, "Grade A");
+        } else if (grade >= 80) {
+            JOptionPane.showMessageDialog(null, "Grade B");
+        } else if (grade >= 70) {
+            JOptionPane.showMessageDialog(null, "Grade C");
+        } else {
+            JOptionPane.showMessageDialog(null, "Failed");
+        }
+    }
+}
