c20e93801fa8099fa775d9d4ed43b3ee5aae0ba8
Vitor Serra <vserra.contact@gmail.com>
2026-01-12 15:04:03 -0300

    exercises(java): while.

---
+++ While.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+public class While {
+    public static void main(String[] args) {
+
+        double grade1, grade2;
+        int counter = 1;
+
+        while (counter <= 5) {
+            grade1 = Double.parseDouble(
+                    JOptionPane.showInputDialog("Enter grade 1")
+            );
+            grade2 = Double.parseDouble(
+                    JOptionPane.showInputDialog("Enter grade 2")
+            );
+
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Average: " + (grade1 + grade2) / 2
+            );
+
+            counter = counter + 1;
+        }
+    }
+}
